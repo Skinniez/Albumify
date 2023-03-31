5f34c984caec9ccdbc731a6bd73b59bf5741e95a
Jesse Strickland <112838688+Skinniez@users.noreply.github.com>
2023-03-31 18:43:18 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getLoginUrl, getAccessTokenFromUrl } from './auth';
 import spotifyApi, { setAccessToken } from './spotify';
-import htmlToImage from 'html-to-image';
 import { toPng } from 'html-to-image';
+import background from './assets/background.png'
 
+function Error({ message }) {
+  return (
+    <div className="error">
+      <p>{message}</p>
+    </div>
+  );
+}
+
+const handleBackgroundLoad = () => {
+  console.log('Background image loaded.');
+};
 
 function App() {
   const [userTopSongs, setUserTopSongs] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const token = getAccessTokenFromUrl();
@@ -24,6 +36,7 @@ function App() {
       setUserTopSongs(response.items);
     } catch (error) {
       console.error('Error fetching user top songs:', error);
+      setError('Error fetching user top songs');
     }
   };
 
@@ -35,22 +48,42 @@ function App() {
           <div className="song-name">{song.name}</div>
           <div className="song-artist">{song.artists[0].name}</div>
         </div>
-        <img className="song-image" src={song.album.images[0]?.url} alt={song.name} />
       </div>
     ));
   };
 
   const generatePoster = () => {
     const node = document.getElementById('songs');
+    const backgroundImage = document.querySelector('.background-image');
   
-    toPng(node).then((dataUrl) => {
-      const link = document.createElement('a');
-      link.href = dataUrl;
-      link.download = 'poster.png';
-      link.click();
-    }).catch((error) => {
+    try {
+      toPng(backgroundImage).then((backgroundDataUrl) => {
+        toPng(node, { backgroundColor: null }).then((dataUrl) => {
+          const canvas = document.createElement('canvas');
+          canvas.width = 971;
+          canvas.height = 1500;
+          const context = canvas.getContext('2d');
+  
+          const background = new Image();
+          background.src = backgroundDataUrl;
+          background.onload = () => {
+            context.drawImage(background, 0, 0, canvas.width, canvas.height);
+            const poster = new Image();
+            poster.src = dataUrl;
+            poster.onload = () => {
+              context.drawImage(poster, 0, 0, canvas.width, canvas.height);
+              const downloadLink = document.createElement('a');
+              downloadLink.href = canvas.toDataURL('image/png');
+              downloadLink.download = 'poster.png';
+              downloadLink.click();
+            };
+          };
+        });
+      });
+    } catch (error) {
       console.error('Error generating poster:', error);
-    });
+      setError('Error generating poster');
+    }
   };
   
 
@@ -58,10 +91,17 @@ function App() {
     <div className="App">
       {userTopSongs ? (
         <div>
+          <img
+            src={background}
+            alt="Background"
+            className="background-image"
+            onLoad={handleBackgroundLoad}
+          />
           <div id="songs">
             {renderTopSongs()}
           </div>
           <button onClick={generatePoster}>Generate Poster</button>
+          {error && <Error message={error} />}
         </div>
       ) : (
         <a href={getLoginUrl()} className="login-button">
